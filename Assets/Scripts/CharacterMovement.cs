da28e609ee1217bb9cbc9e2b92875e167c6df201
Fabián E. García <garciafabianenrique79@gmail.com>
2025-04-10 10:47:05 -0300

    inicio de animación player

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,13 +5,16 @@ public class CharacterMovement : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 3f;
     private Rigidbody2D _playerRb;
-    private Vector2 _moveInput;      
+    private Vector2 _moveInput;
+    private Animator _playerAnimator;
+
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _playerRb = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,6 +23,10 @@ public class CharacterMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         _moveInput = new Vector2(moveX, moveY).normalized;
+
+        _playerAnimator.SetFloat("Horizontal", moveX);
+        _playerAnimator.SetFloat("Vertical", moveY);
+        _playerAnimator.SetFloat("Speed", _moveInput.sqrMagnitude);
     }
 
     private void FixedUpdate()
